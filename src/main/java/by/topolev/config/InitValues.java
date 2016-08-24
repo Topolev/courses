4543fc177ad53def65220d3e399ae554a4cd88fb
Topolev <i.topolev.vladimir@gmail.com>
2016-08-24 06:00:49 -0300

    Correct InitValues

--- src/main/java/by/topolev/config/InitValues.java
+++ src/main/java/by/topolev/config/InitValues.java
@@ -14,26 +14,33 @@ public class InitValues {
 	private static Map<String, String> defaultValues = new HashMap<String, String>();
 	private static Properties properties = null;
 	private static final Logger LOG = LoggerFactory.getLogger(InitValues.class);
-	
+
 	static {
 		defaultValues.put("pathUploadImage", "upload/");
+
+	}
+
+	public InitValues() {
 		Properties property = new Properties();
-		try {			
+		try {
 			FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
 			property.load(fis);
 			properties = property;
 			LOG.debug("Properties file upload success");
-			LOG.debug(String.format("Test property file, pathUploadImage = %s", property.getProperty("pathUploadImage")));
+			LOG.debug(
+					String.format("Test property file, pathUploadImage = %s", property.getProperty("pathUploadImage")));
 		} catch (IOException e) {
 			LOG.debug("Property file config.properties isn't excited or problem with loading");
-		}	
+		}
 	}
-	
-	public static String getValue(String nameValue){
-		if (!defaultValues.containsKey(nameValue)) throw new IllegalArgumentException();
-		if (properties == null){
+
+	public static String getValue(String nameValue) {
+		if (!defaultValues.containsKey(nameValue))
+			throw new IllegalArgumentException();
+		if (properties == null) {
 			return defaultValues.get(nameValue);
-		} else{
+		} else {
+			LOG.debug(String.format("To get value '%s'. Result: '%s'", nameValue, properties.getProperty(nameValue)));
 			properties.getProperty(nameValue);
 		}
 		return null;
